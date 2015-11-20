1de2f38bc60792691382fbfaf685519b08b3d1ad
mayinggang <mayg@pingfang.net>
2015-11-20 11:33:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/maps/User.java
@@ -0,0 +1,15 @@
+package com.example.administrator.maps;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2015/11/19.
+ */
+public class User extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user);
+    }
+}
